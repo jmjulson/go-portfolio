32f4cb280e61feba0a81199bec93b610db19acdd
jordanjulson <jordan.julson@nike.com>
2021-02-08 23:42:13 -0800

    Doh: Add Comments for next session

--- site.go
+++ site.go
@@ -16,6 +16,8 @@ var templates = template.Must(template.ParseGlob("web/templates/*.html"))
 var validPath = regexp.MustCompile("^/(home)/([a-zA-Z0-9]+)$")
 
 func loadPage(title string) *Page {
+	// TODO: Make this actually useful. Starting block for loading from data/* files
+	// TODO: Figure out how to start incorporating nested templates
 	return &Page{PageTitle: title}
 }
 
@@ -28,20 +30,21 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p := loadPage("Page Name")
-	// TODO: get information from data/*
+	// TODO: get actual information from loadPage
 	// TODO: Add proper error handling
     renderTemplate(w, "home.html", p)
 }
 
 func makeHandler(fn func(ww http.ResponseWriter, rr *http.Request, text string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// TODO: Figure out what to do about this...
 		m := "text"
 		fn(w, r, m)
 	}
 }
 
 func main() {
-    //tmpl := template.Must(template.ParseFiles("web/templates/home.html"))
+	// TODO: make the first param here dynamic. Make it file driven.
     http.HandleFunc("/home/", makeHandler(viewHandler))
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
