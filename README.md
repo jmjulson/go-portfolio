aa03ca3a706bead91dd0a57d4050a3beb2655967
jordanjulson <jordan.julson@nike.com>
2021-02-08 22:58:06 -0800

    refactor: remove fat, simplify for project needs

--- README.md
+++ README.md
@@ -6,16 +6,25 @@ run app
 ```./bin/site```
 
 ## Next Steps
-###### Convert HTML Pages to .tmpl
- ```https://golang.org/pkg/html/template/```
- 
-Create header, nav and footer templates
+[X] Try to incoperate the makeHandler with the above.
+[X] Try to move most of this out of main. Make it a single line. Like the example.
+[] Read more about patterns around HanlderFunc and HandleFunc
+[] Try to make the handle func have a URL path related to the file.
+[] Try to make the files drive page path - // 	http.HandleFunc("/home/", makeHandler(viewHandler)) 
+[] Try  to use templating to generate headers, footers and components ( Nav, "ads") {{header}}
+	[] This might be called nested templates - Verify
+	[] https://www.calhoun.io/intro-to-templates-p2-actions/
+[] What is routing? Does this project need it?
+[] Much later ... autoreloading? Maybe?
+[] Think more about how to incoperate more elemets - use an API to generate content in a table
 
 ###### Build a Homepage using "custom" JS, HTML, CSS
 include flexbox in CSS ```https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox```
 
 
 ## Other Things To Read
-###### https://golang.org/pkg/net/http/
-###### https://golang.org/pkg/html/template/
-###### https://golang.org/doc/faq#Pointers
+###### Resource: https://golang.org/pkg/net/http/
+###### Resource: https://golang.org/pkg/html/template/
+###### Resource: https://golang.org/doc/faq#Pointers
+###### Resource: https://www.calhoun.io/
+###### Examples: https://course.gowebexamples.com/?utm_source=banner
--- site.go
+++ site.go
@@ -2,32 +2,21 @@ package main
 
 import (
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"regexp"
 )
 
 type Page struct {
-	Title string
-	Body []byte
+	// TODO: Build Struct to Handle data/* files
+	PageTitle string
 }
 
-var templates = template.Must(template.ParseGlob("web/templates/*.tmpl"))
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+var templates = template.Must(template.ParseGlob("web/templates/*.html"))
+var validPath = regexp.MustCompile("^/(home)/([a-zA-Z0-9]+)$")
 
-func (p *Page) save() error {
-	filename := "bin/" + p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := "bin/" + title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
+func loadPage(title string) *Page {
+	return &Page{PageTitle: title}
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
@@ -38,48 +27,21 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-    p, err := loadPage(title)
-    if err != nil {
-        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-        return
-    }
-    renderTemplate(w, "view.tmpl", p)
+	p := loadPage("Page Name")
+	// TODO: get information from data/*
+	// TODO: Add proper error handling
+    renderTemplate(w, "home.html", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-    p, err := loadPage(title)
-    if err != nil {
-        p = &Page{Title: title}
-    }
-    renderTemplate(w, "edit.tmpl", p)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-    body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-    err := p.save()
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+func makeHandler(fn func(ww http.ResponseWriter, rr *http.Request, text string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w, r)
-			return
-		}
-		fn(w, r, m[2])
+		m := "text"
+		fn(w, r, m)
 	}
 }
 
 func main() {
-	http.HandleFunc("/view/", makeHandler(viewHandler))
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+    //tmpl := template.Must(template.ParseFiles("web/templates/home.html"))
+    http.HandleFunc("/home/", makeHandler(viewHandler))
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
